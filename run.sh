ad6c838e9b97f58085b99713904ab5852c56c770
Anibal Kolker <anibaldk@gmail.com>
2020-06-01 12:06:08 +0200

    Simplified run. Automatically attempts to download the image

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-docker run --rm -v ~/Development/Repositories/blog:/srv/jekyll -p 4000:4000 -it anibal/jekyll 
-#JEKYLL_VERSION=3.8
-#docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/minimal:$JEKYLL_VERSION jekyll build
-#docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:4.0.0 jekyll build
+JEKYLL_VERSION=4.0.0
+docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION /bin/bash
